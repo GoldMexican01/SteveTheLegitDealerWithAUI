f82dd63427281197be06b6c2218005cfc9f2125c
Ollie Pearson <Ollie170@outlook.com>
2018-09-17 10:24:25 +0100

    Lol

---
+++ SteveTheLegitDealerWithAUI/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Sub Main()
+        Dim cardsuit As Integer
+        Dim cardnum As Integer
+        Dim suit As String
+        Dim card As String
+        Console.BackgroundColor = ConsoleColor.White
+        For deal = 1 To 6
+            Randomize()
+            cardsuit = Int(4 * Rnd() + 1) 'this generates the suit
+            Randomize()
+            cardnum = Int(13 * Rnd() + 1)
+
+            If cardsuit = "1" Then 'This gives the suit number a friendly name and a color
+                suit = "Hearts"
+                Console.ForegroundColor = ConsoleColor.DarkRed
+            ElseIf cardsuit = "2" Then
+                suit = "Diamonds"
+                Console.ForegroundColor = ConsoleColor.DarkRed
+            ElseIf cardsuit = "3" Then
+                suit = "Clubs"
+                Console.ForegroundColor = ConsoleColor.Black
+            ElseIf cardsuit = "4" Then
+                suit = "Spades"
+                Console.ForegroundColor = ConsoleColor.Black
+            End If
+
+            If cardnum = "1" Then 'This gives the card number a friendly name
+                card = "Ace"
+            ElseIf cardnum = "11" Then
+                card = "Jack"
+            ElseIf cardnum = "12" Then
+                card = "Queen"
+            ElseIf cardnum = "13" Then
+                card = "King"
+            Else
+                card = cardnum
+            End If
+
+            Console.WriteLine(card & " of " & suit)
+        Next
+        Console.ReadLine()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
---
+++ SteveTheLegitDealerWithAUI/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DaveTheDealerWithAUI")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DaveTheDealerWithAUI")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("07ba2bde-10c5-4506-929e-04928a54cb60")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
